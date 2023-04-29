4a39e693019d91c163decd9ece8a017083c296d9
priyanka kasera <priyankakasera13@gmail.com>
2023-04-29 17:03:18 +0530

    #1 initial setup

---
+++ src/main/java/com/ecommerce/usermgmtapi/UsermgmtapiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.usermgmtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermgmtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermgmtapiApplication.class, args);
+	}
+
+}
